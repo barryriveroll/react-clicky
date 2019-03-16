0cd885e2dfb02c4803aa346e565933f81605d97c
bareseph <barryriveroll@gmail.com>
2019-03-16 18:44:15 -0400

    Updates

---
+++ precache-manifest.8264607530aa26fcde8222ceda5a7546.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f60053edc30175445e5",
+    "url": "/react-clicky/static/js/runtime~main.e19a1b38.js"
+  },
+  {
+    "revision": "ff7aa19254670b298bde",
+    "url": "/react-clicky/static/js/main.bc7bdca9.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/react-clicky/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "ff7aa19254670b298bde",
+    "url": "/react-clicky/static/css/main.72573593.chunk.css"
+  },
+  {
+    "revision": "721a275a45f10638fbac8f77656310b2",
+    "url": "/react-clicky/index.html"
+  }
+];
